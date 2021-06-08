3be80df47439cc08d7f549b33e65eb3c2df8c1c6
Rahul Singh <rahulsingh21247@gmail.com>
2021-06-08 12:21:32 +0530

    updated stageDetails

--- server/app/controllers/stageDetail.controller.js
+++ server/app/controllers/stageDetail.controller.js
@@ -1,11 +1,12 @@
 const stageDetail = require("../models/stageDetail.model.js");
+const stagesController = require("./stage.controller");
 
 
 
-// create stage
+// create stage ******************************
 
 exports.createStage = async (req, res) => {
-  // Validate request
+  // Validate request ----------------------------
   if (!req.body) {
     res.status(400).send({
       message: "Content can not be empty!"
@@ -15,63 +16,122 @@ exports.createStage = async (req, res) => {
   const stages = req.body;
 
   
-    //create stage table
-    const createStage =  await  stageDetail.createStage("stage_table");
+    //create stage table ----------------------------------
+    await jobController.createJob(jobName);
+  //const createStage =  await  stageDetail.createStage("stage_table");
 
-    // find stage name
+    // find stage name -----------------------------------
   const findStage= [];
   for(name in stages) {
     findStage.push(name);
   }
-
+  await stagesController.createAllStages(findStage, 1);
+  
+   // create stageDetail ------------------------
   try{
-  var k =101;
-  for(var i=0; i<findStage.length; i++) {
-    for(var j=0; j<findStage[i].length; j++){
-      // create stageDetail
-      for(column in stages[findStage[i]][j]) {
-        const stageRecordNew = {};
-        stageRecordNew.jobId = 1;
-        stageRecordNew.stageId = k;
-        stageRecordNew.stageDetailId =j;
-        stageRecordNew.property = column;
-        stageRecordNew.property_value = stages[findStage[i]][j][column];
-        console.log("\nrecord == ", stageRecordNew);
-        const createStageRecord =  await stageDetail.createStageRecord("stage_table", stageRecordNew);
-           Promise.all(createStageRecord);
+    var k =101;
+    for(var i=0; i<findStage.length; i++) {
+      for(var j=0; j<findStage[i].length; j++){
+      await insertFunction(stages[findStage[i]][j], j, k, 1);
       }
+      k++;
     }
-    k++;
+    res.status(200).send({
+      message: "entery successfully"
+    })} catch (err) {
+    res.status(500).send({
+      message:
+        err.message || "Some error occurred while entering stage details all Stages."
+    });
   }
-  
-  res.status(200).send({
-    message: "entery successfully"
-  })
-  
- }
- catch (err) {
-  res.status(500).send({
-    message:
-      err.message || "Some error occurred while entering stage details all Stages."
-  });
-}
 };
 
-exports.getAll = async(req, res, next)=>{
-  stageDetail.getAll(req, (err,data) => {
-    if (err)
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while retrieving stageDetails."
-      });
-    else res.send(data);
-  });
-}
+// **********************************
 
-exports.updateStageDetail = (req, res, next)=>{
-  if (!req.params.stageDetailId || !req.params.stageId || !req.body) {
+// ********************get all stageDetails ***************************
+exports.getAll = async(req, res, next)=>{
+  try{
+    await stageDetail.getAll(req, (err,data) => {
+        if (err)
+          res.status(500).send({
+            message:
+              err.message || "Some error occurred while retrieving stageDetails."
+          });
+        else res.send(data);
+      });
+    }catch (err) {
+      console.log(err);
+    }
+}
+// **************************END of get all stageDetails*******************************
+
+//  *************************update stageDetails***************************************
+exports.updateStageDetail = async (req, res, next)=>{
+  if (!req.params.stageDetailId || !req.params.stageId || !req.params.jobId || !req.body) {
     res.status(400).send({
       message: "stageDetailId  can not be empty!"
     });
+}
+
+try{
+  const deleteStageDetail = await deleteFunction(req.params, res);
+  const insertUpdates = await insertFunction(req.body, req.params.stageDetailId, req.params.stageId, req.params.jobId);
+  }catch(err){
+    console.log(err);
   }
 }
+// ************************END of update stageDetails****************************************
+
+//  *************************delete stageDetails***************************************
+exports.delete = (req, res) => {
+  deleteFunction(req.params, res);
+ };
+// ***************************END of delete stageDetails*************************************
+
+
+// *************************** helper functions*************************************
+
+// insert stageDetails into table ---------------------
+ const insertFunction= async (data, stageDetailId, stageId, jobId) => {
+      try{
+      for(column in data) {
+        const stageRecordNew = {};
+        stageRecordNew.jobId = jobId;
+        stageRecordNew.stageId = stageId;
+        stageRecordNew.stageDetailId =stageDetailId;
+        stageRecordNew.property = column;
+        stageRecordNew.property_value = data[column];
+        console.log("\nrecord == ", stageRecordNew);
+        const createStageRecord =  await stageDetail.createStageRecord("stage_table", stageRecordNew);
+          Promise.all(createStageRecord);
+      }
+    } catch (err){
+      console.log(err);
+    }
+ }
+
+ // delete stageDetail --------------------------------
+ const deleteFunction = async (reqParams, res) => {
+    try{
+      await stageDetail.remove(reqParams, (err, data) => {
+      if (err) {
+        if (err.kind === "not_found") {
+          res.status(404).send({
+            message: `Not found stageDetail.`
+          });
+        } else {
+          res.status(500).send({
+            message: "Could not delete stageDetail with "
+          });
+        }
+      } else res.send({ message: `stageDetail was deleted successfully!` });
+    });
+  }catch(err) {
+    console.log(err);
+  }
+}
+
+
+//ishika ke controllers
+
+
--- server/app/models/stageDetail.model.js
+++ server/app/models/stageDetail.model.js
@@ -1,3 +1,4 @@
+const { json } = require("body-parser");
 const sql = require("./db.js");
 
 const stageDetail = function(stageDetail){
@@ -46,5 +47,46 @@ stageDetail.getAll = (req, result)=>{
    }
   })
 }
+
+stageDetail.updateById = async (reqParams, reqBody, result) => {
+
+  let qry = "UPDATE stage_table SET ?" + reqBody + " WHERE stageDetailId = " + reqParams.stageDetailId +
+            " AND stageId = " + reqParams.stageId + " AND jobId = " + reqParams.jobId ;
+            console.log(qry);
+            return ;
+}
+
+stageDetail.remove = async (reqParams, result) => {
+  console.log(reqParams);
+  let qryDelete = "DELETE FROM stage_table " + " WHERE stageDetailId = " + reqParams.stageDetailId +
+  " AND stageId = " + reqParams.stageId + " AND jobId = " + reqParams.jobId ;
+  console.log(qryDelete);
+  try{
+    await sql.query(qryDelete, (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+      }
+
+      if (res.affectedRows == 0) {
+        // not found stageDetail 
+        result({ kind: "not_found" }, null);
+        return;
+      }
+
+      console.log("deleted stageDetail: ");
+      result(null, res);
+    });
+  }catch(err){
+    console.log("error: ", err);
+  }
+}
+
+
+// ishika ke modules
+
+
+
 
 module.exports = stageDetail;
